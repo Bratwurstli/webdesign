3eab1649f02abc5632ef9a8b71f65e444442d7ea
Sven Mengler <s.s.mengler@gmx.net>
2017-05-14 22:05:18 +0200

    changed impressum completely and finished styling

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
-// footer
+// impressum
 
-// first impressum is not shown
+// first impressum is hidden
 var impHide = 1;
+
+function impressum() {
+	if (impHide == 1) {
+		document.getElementById("impressum").style.display = "block";
+		impHide = 0;
+	}
+	else if (impHide == 0) {
+		document.getElementById("impressum").style.display = "none";
+		impHide = 1;
+	}
+}
