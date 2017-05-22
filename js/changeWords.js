8033459cd25aacd805c0cc2f3a53a9242d53c7d7
Sven Mengler <s.s.mengler@gmx.net>
2017-05-23 00:56:06 +0200

    finished word animation

--- js/changeWords.js
+++ js/changeWords.js
@@ -2,15 +2,35 @@
 
 // change words
 
-var words = ["Mathematiker", "Stripper", "Pornodarsteller"];
-var i = 1;
+var words = ["Mathematics", "Web Design", "Teaching"];
+var i = 0;
+var myArray = words[i].split("");
 
-function changeWord() {
-	document.getElementById("words").innerHTML = words[i];
-	i += 1;
-	if (i > words.length - 1) {i = 0;}
+function deleteWord(word) {
+
+	if (word.length > 0) {
+		word = word.substring(0,word.length-1);
+		document.getElementById("words").innerHTML = word;
+		setTimeout(function(){deleteWord(word);}, Math.random()*50+50);
+	}
 }
 
-window.setInterval(function(){
-  changeWord();
-}, Math.floor((Math.random() * 2000)) + 2000);
+function writeWord() {
+	if(myArray.length > 0) {
+		document.getElementById("words").innerHTML += myArray.shift();
+		setTimeout(function(){writeWord();}, Math.random()*50+50);
+	}
+}
+
+function changeWord() {
+
+	var word = words[i];
+	deleteWord(word);
+	i += 1;
+	if (i > words.length-1) {i = 0}
+	myArray = words[i].split("");	
+	setTimeout(function(){writeWord();}, 1500);
+}
+
+setTimeout(function(){writeWord();}, 1500);
+window.setInterval(function(){changeWord();}, 6000);
